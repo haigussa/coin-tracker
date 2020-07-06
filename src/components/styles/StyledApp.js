6e370c8181e204adf2b596756e0bb76bb5135028
Gussa Haile <haigussa@gmail.com>
2020-07-07 00:21:44 +0200

    ready for deploy

--- src/components/styles/StyledApp.js
+++ src/components/styles/StyledApp.js
@@ -1,16 +1,13 @@
 import styled from 'styled-components'
 
 const StyledApp = styled.div`
-
   text-align: center;
   background-color: #F4E7E3;
   padding: 2rem 1rem;
   color: #00214D;
+  margin:auto; */
   h1{
     color: #00214D;
   }
-
-
 `
-
 export default StyledApp;
